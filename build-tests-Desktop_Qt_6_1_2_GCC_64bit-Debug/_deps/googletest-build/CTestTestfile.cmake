bce181ead98d07b45a3f8fc32878b10d9c5680a8
petlenz <peterlenz89.pl@gmail.com>
2022-05-06 11:00:25 +0200

    Contributing

--- build-tests-Desktop_Qt_6_1_2_GCC_64bit-Debug/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/peter/SynologyDrive/Drive/tmech/build-tests-Desktop_Qt_6_1_2_GCC_64bit-Debug/_deps/googletest-src
-# Build directory: /home/peter/SynologyDrive/Drive/tmech/build-tests-Desktop_Qt_6_1_2_GCC_64bit-Debug/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
